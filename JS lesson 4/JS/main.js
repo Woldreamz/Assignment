b90ebe7c7b9ec172abaa7d2ee10af2d601cb3f3e
Woldreamz <woldreamz@gmail.com>
2024-02-19 15:02:50 +0100

    KingJames Rules the West

---
+++ JS lesson 4/JS/main.js
@@ -0,0 +1,56 @@
+//Strings Concatenating
+/* We basically use concatenations for numbers */
+
+const Age = 28;
+const babeAge = 26
+
+const her = "Her";
+const she = "She";
+
+console.log(Age + babeAge);
+console.log(her + she);
+console.log(her);
+console.log("The" + " Lord loves " + "the gates of " + "zion " + "more than " + "all the dwellings " + " of " + " Jacob ");
+console.log("Her Name is" + "Teniola");
+
+const myHouse = "London";
+const myName = "Nenye";
+const myAge = "26";
+
+console.log('I stay in ' + myHouse + ' with my girlfriend ' + myName);
+console.log(`I have a new house with ${myName} in ${myHouse}`);
+console.log(`I stay in ${myHouse} with ${myName} and she is ${myAge}`);
+
+// Create 20 variables of strings, numbers, booleans and concatenate them into one short essay.
+
+//JavaScript If/else statements
+//Create a JavaScript function that that limits the lads below 18 from getting into the dating site.
+
+const age = 19;
+
+if (age > 18) {
+    console.log("Enter your majesty");
+} else {
+    console.log("Common commot for here");
+}
+
+const dice1 = 6;
+const dice2 = 6;
+
+if (dice1 == 6 && dice2 == 6) {
+
+    console.log("Now You are the victor");
+} else {
+    console.log("You are not the victor");
+}
+
+const dice4 = 6;
+const dice5 = 3;
+
+if (dice4 === 6 && dice5 === 6) {
+    console.log("Now You are the victor");
+} else {
+    console.log("You are not the victor");
+}
+
+//Create 4 simple game using the if/else statement
